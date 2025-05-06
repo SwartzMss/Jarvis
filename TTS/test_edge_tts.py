4be45040a7a894132cdd70670c60363b4b217094
SwartzMss <swartz_lubel@outlook.com>
2025-05-06 23:07:18 +0800

    不搞ASR和TTS了 准确率太低了 而且极其难调试
    Signed-off-by: SwartzMss <swartz_lubel@outlook.com>

--- TTS/test_edge_tts.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from edge_tts_api import EdgeTTSApi
-
-if __name__ == "__main__":
-    tts = EdgeTTSApi()
-    text = "你好,你是狗子吗"
-    # 同步调用
-    ok = tts.sync_play_text(text)
-    print("同步播放：", ok)
